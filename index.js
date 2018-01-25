8c802b172d3fb36e8c61791d1f34615b66d159ab
SE05193 <frodriguez_gft@bankinter.com>
2018-01-25 16:31:10 +0100

    feat(initial): initial version

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const ccxt = require ('ccxt'),
+    asTable = require ('as-table'),
+    asciichart = require ('asciichart'),
+    log = require ('ololog').configure ({ locate: false });
+require ('ansicolor').nice
+
+let sleep = (ms) => new Promise (resolve => setTimeout (resolve, ms));
+
+(async () => {
+    let exchange = new ccxt.bittrex  ({
+        "apiKey": "508aef57f65e4335a70ec013457f4aaf",
+        "secret": "4ee3786e433f4778bcc87599c03a5208",
+    })
+    try {
+        const orders = await exchange.fetchOrders ()
+        log (asTable (orders.map (order => ccxt.omit (order, [ 'timestamp', 'info' ]))))
+        const order = await exchange.fetchOrder (orders[0]['id'])
+        log (order);
+    } catch (e) {
+        if (e instanceof ccxt.DDoSProtection || e.message.includes ('ECONNRESET')) {
+            log.bright.yellow ('[DDoS Protection] ' + e.message)
+        } else if (e instanceof ccxt.RequestTimeout) {
+            log.bright.yellow ('[Request Timeout] ' + e.message)
+        } else if (e instanceof ccxt.AuthenticationError) {
+            log.bright.yellow ('[Authentication Error] ' + e.message)
+        } else if (e instanceof ccxt.ExchangeNotAvailable) {
+            log.bright.yellow ('[Exchange Not Available Error] ' + e.message)
+        } else if (e instanceof ccxt.ExchangeError) {
+            log.bright.yellow ('[Exchange Error] ' + e.message)
+        } else if (e instanceof ccxt.NetworkError) {
+            log.bright.yellow ('[Network Error] ' + e.message)
+        } else {
+            throw e;
+        }
+    }
+}) ();
